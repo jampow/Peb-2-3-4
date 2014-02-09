0ff9f909a6ab228794175b4be01da1ee11e27413
mkane89 <kanemeghan@gmail.com>
2014-02-09 18:46:22 -0500

    1.0.0 version released to Pebble app store

--- src/pebtwothreefour.c
+++ src/pebtwothreefour.c
@@ -1,28 +1,38 @@
 #include <pebble.h>
 #include <time.h>
 
-// pointer declared for Window element
+// pointers to window layer and other UI layers
 static Window *window;
-
-// used to show any standard string of characters in a predefined area
 static TextLayer *text_layer;
+static TextLayer *units_layer;
+static ActionBarLayer *abl_action;
 
-char buffer[] = "00:00";
-char bpmtext[32] = "80";
+// icons for action bar layer
+static GBitmap *gb_arrow_up;
+static GBitmap *gb_arrow_down;
+static GBitmap *gb_play;
+static GBitmap *gb_pause;
 
-AppTimer *timer;
+// bpm defaulted to 120 when app opens
+int bpm = 120;
+static int min_bpm = 40;
+static int max_bpm = 240;
+char bpmtext[] = "120";
+char bpmlabel[] = "bpm";
 
-
-int bpm = 80;
+// timers
+AppTimer *betweenbeats_timer;
+AppTimer *longupclick_timer;
+AppTimer *longdownclick_timer;
 
 // time in between vibrations, in ms
 int delta = 500;
 
-bool vibrating = 0;
+//
+bool is_vibrating = 0;
 
-// Vibe pattern: ON for 50ms -- seems to be shortest interval that can be felt
-//static const uint32_t const segments[] = {50};
-uint32_t segments[1] = {50};
+// Vibe pattern: ON for 45ms -- seems to be shortest interval that can be felt and is relevant at each tempo between 40 and 240 bpm
+static const uint32_t const segments[] = {45};
 VibePattern pat = {
     // uint32_t type
     .durations = segments,
@@ -30,157 +40,241 @@ VibePattern pat = {
     .num_segments = ARRAY_LENGTH(segments),
 };
 
-void timer_callback(void *data) {
+
+void betweenbeats_timer_callback(void *data) {
+    //
     vibes_enqueue_custom_pattern(pat);
-    //vibes_short_pulse();
     
-    //Register next execution
-    timer = app_timer_register(delta, (AppTimerCallback) timer_callback, NULL);
+    // Register next execution
+    betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+}
+
+
+
+char* itoa(int val, int base){
+    static char buf[32] = {0};
+    int i = 30;
+    for(; val && i ; --i, val /= base)
+        buf[i] = "0123456789abcdef"[val % base];
+    return &buf[i+1];
+}
+
+void longupclick_timer_callback(void *data) {
+    
+    if (bpm < max_bpm){
+        bpm++;
+        char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
+        strcpy(bpmChar, itoa(bpm, 10));
+        text_layer_set_text(text_layer, bpmChar);
+        free(bpmChar);
+        
+        //Register next execution
+        longupclick_timer = app_timer_register(50, (AppTimerCallback) longupclick_timer_callback, NULL);
+    }
+    
+}
+
+void longdownclick_timer_callback(void *data) {
+    
+    if (bpm > min_bpm){
+        bpm--;
+        char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
+        strcpy(bpmChar, itoa(bpm, 10));
+        text_layer_set_text(text_layer, bpmChar);
+        free(bpmChar);
+        
+        //Register next execution
+        longdownclick_timer = app_timer_register(50, (AppTimerCallback) longdownclick_timer_callback, NULL);
+    }
+    
 }
 
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  //text_layer_set_text(text_layer, "Metronome stopped");
     
-    if (vibrating){
-        app_timer_cancel(timer);
-        vibrating = 0;
+    // if already is_vibrating, cancel vibration and change ActionBarLayer icon from play icon to pause icon
+    if (is_vibrating){
+        app_timer_cancel(betweenbeats_timer);
+        is_vibrating = 0;
+
+        action_bar_layer_set_icon(abl_action, BUTTON_ID_SELECT, gb_play);
     }
     else{
+        // sets time between beat vibrations according to bpm set
         delta = (60 * 1000)/ bpm;
-        timer = app_timer_register(delta, (AppTimerCallback) timer_callback, NULL);
-        vibrating = 1;
+        betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+        is_vibrating = 1;
+        
+        action_bar_layer_set_icon(abl_action, BUTTON_ID_SELECT, gb_pause);
     }
 }
 
-char* itoa(int val, int base){
-    
-    static char buf[32] = {0};
-    
-    int i = 30;
-    
-    for(; val && i ; --i, val /= base)
-        
-        buf[i] = "0123456789abcdef"[val % base];
-    
-    return &buf[i+1];
-    
-}
+
 
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  app_timer_cancel(timer);
     
-    bpm = bpm + 1;
-    char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
-    strcpy(bpmChar, itoa(bpm, 10));
-    text_layer_set_text(text_layer, bpmChar);
-    free(bpmChar);
-    
+    if (bpm < max_bpm){
+        bpm++;
+        char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
+        strcpy(bpmChar, itoa(bpm, 10));
+        text_layer_set_text(text_layer, bpmChar);
+        free(bpmChar);
+        
+        if (is_vibrating){
+            app_timer_cancel(betweenbeats_timer);
+            delta = (60 * 1000)/ bpm;
+            betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+        }
+    }
 }
-
-
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    bpm = bpm - 1;
-    char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
-    strcpy(bpmChar, itoa(bpm, 10));
-    text_layer_set_text(text_layer, bpmChar);
-    free(bpmChar);
+    
+    if (bpm > min_bpm){
+        bpm--;
+        char * bpmChar = malloc(strlen(itoa(bpm,10))+1);
+        strcpy(bpmChar, itoa(bpm, 10));
+        text_layer_set_text(text_layer, bpmChar);
+        free(bpmChar);
+        
+        if (is_vibrating){
+            app_timer_cancel(betweenbeats_timer);
+            delta = (60 * 1000)/ bpm;
+            betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+        }
+    }
+
 }
 
-static void click_config_provider(void *context) {
+void up_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+    
+    if (is_vibrating){
+        app_timer_cancel(betweenbeats_timer);
+    }
+    
+    longupclick_timer = app_timer_register(50, (AppTimerCallback) longupclick_timer_callback, NULL);
+}
+void up_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
+   
+    app_timer_cancel(longupclick_timer);
+    if (is_vibrating){
+        delta = (60 * 1000)/ bpm;
+        betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+    }
+}
+
+void down_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+    
+    if (is_vibrating){
+        app_timer_cancel(betweenbeats_timer);
+    }
+    
+    longdownclick_timer = app_timer_register(50, (AppTimerCallback) longdownclick_timer_callback, NULL);
+}
+
+void down_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
+    
+    app_timer_cancel(longdownclick_timer);
+    if (is_vibrating){
+        delta = (60 * 1000)/ bpm;
+        betweenbeats_timer = app_timer_register(delta, (AppTimerCallback) betweenbeats_timer_callback, NULL);
+    }
+}
+
+static void abl_click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+    
+    window_long_click_subscribe(BUTTON_ID_UP, 300, up_long_click_handler, up_long_click_release_handler);
+    window_long_click_subscribe(BUTTON_ID_DOWN, 300, down_long_click_handler, down_long_click_release_handler);
 }
-
-
-/*static void handle_tick(struct tm *tick_time, TimeUnits units_changed) {
-    APP_LOG(APP_LOG_VERBOSE, "Time flies!");
-    
-}*/
-
-/*static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
-{
-    //Here we will update the watchface display
-    
-    //Format the buffer string using tick_time as the time source
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-    
-    //Change the TextLayer text to show the new time!
-    text_layer_set_text(text_layer, buffer);
-}*/
-
 
 // adds the creation of Window's elements
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
- // GRect bounds = layer_get_bounds(window_layer);
     
+    Layer *window_layer = window_get_root_layer(window);
     
-   /* text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+    abl_action = action_bar_layer_create();
+    action_bar_layer_add_to_window(abl_action, window);
+    action_bar_layer_set_click_config_provider(abl_action, abl_click_config_provider);
     
+    // create bitmap icons for action bar
+    gb_arrow_up = gbitmap_create_with_resource(RESOURCE_ID_ARROW_UP);
+    gb_arrow_down = gbitmap_create_with_resource(RESOURCE_ID_ARROW_DOWN);
+    gb_play = gbitmap_create_with_resource(RESOURCE_ID_PLAY);
+    gb_pause = gbitmap_create_with_resource(RESOURCE_ID_PAUSE);
     
-   
+    // set
+    action_bar_layer_set_icon(abl_action, BUTTON_ID_UP, gb_arrow_up);
+    action_bar_layer_set_icon(abl_action, BUTTON_ID_DOWN, gb_arrow_down);
+    action_bar_layer_set_icon(abl_action, BUTTON_ID_SELECT, gb_play);
     
-  text_layer_set_text(text_layer, bpmtext);
+    GRect bounds = layer_get_bounds(window_layer);
+    text_layer = text_layer_create((GRect) { .origin = { 0, 48 }, .size = { bounds.size.w, 168 } });
     
-    
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));*/
-    
-    
-    
-    
-    text_layer = text_layer_create(GRect(0, 53, 132, 168));
+    //text_layer = text_layer_create(GRect(0, 45, 132, 168));
     text_layer_set_background_color(text_layer, GColorClear);
     text_layer_set_text_color(text_layer, GColorBlack);
     text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
     text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-    
     text_layer_set_text(text_layer, bpmtext);
     
     layer_add_child(window_get_root_layer(window), (Layer*) text_layer);
     
+    
+    //units_layer = text_layer_create(GRect(0, 100, 132, 30));
+    units_layer = text_layer_create((GRect) { .origin = { 0, 92 }, .size = { bounds.size.w, 35 } });
+    text_layer_set_background_color(units_layer, GColorClear);
+    text_layer_set_text_color(units_layer, GColorBlack);
+    text_layer_set_text_alignment(units_layer, GTextAlignmentCenter);
+    text_layer_set_font(units_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+    
+    text_layer_set_text(units_layer, bpmlabel);
+    layer_add_child(window_get_root_layer(window), (Layer*) units_layer);
 }
 
 // safely destroys the Window's elements
 static void window_unload(Window *window) {
     
-    app_timer_cancel(timer);
-  text_layer_destroy(text_layer);
+    app_timer_cancel(betweenbeats_timer);
+    
+    text_layer_destroy(text_layer);
+    action_bar_layer_destroy(abl_action);
+    gbitmap_destroy(gb_arrow_up);
+    gbitmap_destroy(gb_arrow_down);
+    gbitmap_destroy(gb_play);
+    gbitmap_destroy(gb_pause);
 }
+
 
 static void init(void) {
     
     
     // initialize app elements here
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
+      window = window_create();
+      window_set_click_config_provider(window, abl_click_config_provider);
+      window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+      });
+      const bool animated = true;
     
     // slides window into view
-  window_stack_push(window, animated);
+    window_stack_push(window, animated);
     
-    //tick_timer_service_subscribe(MINUTE_UNIT, (TickHandler) tick_handler);
-    
-    //tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
 static void deinit(void) {
     window_destroy(window);
-    //tick_timer_service_unsubscribe();
+    
+    
 }
 
 int main(void) {
   init();
 
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
     
   app_event_loop();
   deinit();
